3612693a0b3fc47cc1ca335da1ca26807b633549
Max <114068285+MaxJ02@users.noreply.github.com>
2023-03-13 11:34:37 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
+/********************************************************************************
+* main.c: Demonstration of a 32-bit CPU in progress, based on the AVR architecture.
+********************************************************************************/
+#include "cpu_controller.h"
 
-#include <avr/io.h>
-
-
+/********************************************************************************
+* main: Controls the program flow of the 32-bit processor via keyboard input from the user.
+********************************************************************************/
 int main(void)
 {
-    /* Replace with your application code!! */
-    while (1) 
-    {
-    }
+   cpu_controller_run_by_input();
+   return 0;
 }
-

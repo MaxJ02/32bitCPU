30269dc9592421a0b45cff0af63706c5ec8cf738
Max <114068285+MaxJ02@users.noreply.github.com>
2023-03-16 15:58:03 +0100

    Update alu.h

--- alu.h
+++ alu.h
@@ -40,3 +40,18 @@
 #define ALU_H_
 
 #include "cpu.h"
+
+/********************************************************************************
+* alu: Performs calculation with specified operands and returns the result.
+*      The status flags SNZVC of the referenced status register are updated 
+*      in accordance with the result.
+* 
+*      - operation: The operation to perform (OR, AND, XOR, ADD or SUB).
+*      - a        : First operand.
+*      - b        : Second operand.
+*      - sr       : Reference to status register containing SNZVC flags.
+********************************************************************************/
+uint8_t alu(const uint32_t operation, 
+            const uint32_t a, 
+            const uint32_t b, 
+            uint8_t* sr);
